6d3f354b60b137e1a9e1d10bb7fd4d692c05a729
Radu Vasilescu <vasilescur@gmail.com>
2018-04-26 09:14:35 -0400

    asdasdads

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,25 @@
+import 'package:pfl/pfl.dart';
+
+import 'dart:io';
+
 main(List<String> arguments) {
-    // TODO
+    print('Procedural Footnote Language Interpreter');
+    print('[Version 0.0.1] - (c) Radu Vasilescu, 2018.');
+    print('');
+
+    if (arguments.length < 1) {
+        print('Invalid command. Syntax: dart bin/main.dart filename.pfl');
+        print('');
+        exit(1);
+    }
+
+    String sourceFilePath = arguments[0];
+    Uri sourceFileUri = new Uri.file(sourceFilePath);
+    File sourceFile = new File.fromUri(sourceFileUri);
+
+    List<String> fullSource = sourceFile.readAsLinesSync();
+
+    PflProgram program = new PflProgram(fullSource);
+
+    
 }
---
+++ lib/pfl.dart
@@ -0,0 +1,10 @@
+class PflProgram {
+    List<String> fullSource;
+
+    PflProgram(this.fullSource);
+
+    void execute() {
+        // TODO
+        throw new UnimplementedError();
+    }
+}
